b82cbc421af87a3f6d640f4d22e61dcd861037de
Elliott <elliott@grouloo.fr>
2020-07-31 18:51:03 +0200

    2nd commit

---
+++ precache-manifest.a94056f214f535ff49e4f7c15b0286bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9356a899c22ba93c08d8ffd5e7b2c3d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5163979a35ff11ca5447",
+    "url": "/static/css/2.7fcdf8c4.chunk.css"
+  },
+  {
+    "revision": "c4f053d745e9cefa48ad",
+    "url": "/static/css/main.7c597074.chunk.css"
+  },
+  {
+    "revision": "5163979a35ff11ca5447",
+    "url": "/static/js/2.85328db4.chunk.js"
+  },
+  {
+    "revision": "eaaba1002aad5386d47b37d6e0f0dc8d",
+    "url": "/static/js/2.85328db4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4f053d745e9cefa48ad",
+    "url": "/static/js/main.0dacd077.chunk.js"
+  },
+  {
+    "revision": "baaa868e3263093a06d7",
+    "url": "/static/js/runtime-main.ced797e0.js"
+  }
+]);
